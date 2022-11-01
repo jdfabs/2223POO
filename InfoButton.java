372a56d90a76acb8402ccf7dee30d77454cc8f3f
jdfabs <jdfabs1@gmail.com>
2022-11-01 09:23:24 +0000

    Vitor First Commit

---
+++ InfoButton.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class infoButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class InfoButton extends Actor
+{
+    private GreenfootImage info;
+    //Construtor do botão de info
+    public InfoButton()
+    {
+        info = new GreenfootImage("HelpButton.png");
+        info.scale(100,100);
+        setImage(info);
+    }
+    /**
+     * Act - do whatever the infoButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        help();
+    }
+    public void help()
+    {
+        if(Greenfoot.mouseClicked(this))
+    {
+            Greenfoot.setWorld(MenuInfo());
+        }
+    }
+}
---
+++ MenuInicial.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MenuInicial extends Menu
+{
+    private GreenfootImage menuInicial;
+    private GreenfootSound som;
+    /**
+     * Constructor for objects of class MenuInicial.
+     * 
+     */
+    public MenuInicial()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        
+        
+        menuInicial = new GreenfootImage("MenuInicial.jpeg");
+        menuInicial.scale(1000,1000);
+        setBackground(menuInicial);
+        
+        StartButton play = new StartButton();
+        addObject(play, 534, 300);
+        
+        InfoButton info = new InfoButton();
+        addObject(info, 534, 400);
+        
+        MuteButton mute = new MuteButton();
+        addObject(mute, getWidth()-950, 50);
+                
+        
+
+        
+    }
+    public GreenfootSound somFundo()
+    {
+        return som;
+    }
+    public void stopped(){
+         somFundo().pause();
+    }
+}
